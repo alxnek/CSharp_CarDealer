1d087cd485d9d25dc02c5743fd08ab35a87d4633
Alejandro Jorge <alxnek@gmail.com>
2013-02-27 13:19:36 +0100

    CarDealer update

--- CarDealer/CarDealer.cs
+++ CarDealer/CarDealer.cs
@@ -7,5 +7,37 @@ namespace CarDealerLibraries
 {
     public class CarDealer
     {
+        private List<Vehicle> vehicleList;
+        // Add CustomerList
+
+        public CarDealer(List<Vehicle> vehicleList)
+        {
+            this.vehicleList = vehicleList;
+        }
+
+        public CarDealer()
+        {
+
+        }
+
+        public Vehicle Vehicle
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Add vehicle to the List
+        /// </summary>
+        /// <remarks>Vehicle Class</remarks>
+        public void AddVehicle(Vehicle vehicle)
+        {
+            this.vehicleList.Add(vehicle);
+        }
     }
 }
