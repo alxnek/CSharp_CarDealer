edba75e555fb4c60a5a5613f0ac898092fe12b42
motherdear <christofferrclausen@gmail.com>
2013-02-28 10:35:12 +0100

    Selectable customer information added

--- CarDealer_GUI/MainWindow.xaml.cs
+++ CarDealer_GUI/MainWindow.xaml.cs
@@ -22,39 +22,74 @@ namespace CarDealer_GUI
 			this.InitializeComponent();
             
 			// Insert code required on object creation below this point.
+            select_pri_customer.IsChecked = true;
+            this.textbox_bus_address.IsEnabled = false;
+            this.textbox_bus_phone.IsEnabled = false;
+            this.textbox_bus_seno.IsEnabled = false;
+            this.textbox_bus_fax.IsEnabled = false;
+            this.textbox_bus_contact.IsEnabled = false;
+            this.textbox_bus_company.IsEnabled = false;
+
+            this.textbox_pri_address.IsEnabled = true;
+            this.textbox_pri_phone.IsEnabled = true;
+            this.textbox_pri_name.IsEnabled = true;
+            this.textbox_pri_age.IsEnabled = true;
+            this.textbox_pri_sex.IsEnabled = true;
 		}
 
-        private void select_pri_customer(object sender, RoutedEventArgs e)
+        private void select_pri_customer_Checked(object sender, RoutedEventArgs e)
         {
-            /*use an if statement and visibility statement to control hiding of textboxes 
-             * http://stackoverflow.com/questions/10340342/loading-different-user-control-based-on-radio-button-selection-using-xaml
-             */ 
+            this.textbox_bus_address.IsEnabled = false;
+            this.textbox_bus_phone.IsEnabled = false;
+            this.textbox_bus_seno.IsEnabled = false;
+            this.textbox_bus_fax.IsEnabled = false;
+            this.textbox_bus_contact.IsEnabled = false;
+            this.textbox_bus_company.IsEnabled = false;
+
+            this.textbox_pri_address.IsEnabled = true;
+            this.textbox_pri_phone.IsEnabled = true;
+            this.textbox_pri_name.IsEnabled = true;
+            this.textbox_pri_age.IsEnabled = true;
+            this.textbox_pri_sex.IsEnabled = true;
+        }
+        private void select_bus_customer_Checked(object sender, RoutedEventArgs e)
+        {
+            this.textbox_bus_address.IsEnabled = true;
+            this.textbox_bus_phone.IsEnabled = true;
+            this.textbox_bus_seno.IsEnabled = true;
+            this.textbox_bus_fax.IsEnabled = true;
+            this.textbox_bus_contact.IsEnabled = true;
+            this.textbox_bus_company.IsEnabled = true;
+
+            this.textbox_pri_address.IsEnabled = false;
+            this.textbox_pri_phone.IsEnabled = false;
+            this.textbox_pri_name.IsEnabled = false;
+            this.textbox_pri_age.IsEnabled = false;
+            this.textbox_pri_sex.IsEnabled = false;
         }
 
-        private void select_bus_customer(object sender, RoutedEventArgs e)
+        
+        private void select_veh_car_Checked(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void select_veh_car(object sender, RoutedEventArgs e)
+        private void select_veh_truck_Checked(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void select_veh_truck(object sender, RoutedEventArgs e)
+        private void select_cont_sale_Checked(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void select_cont_sale(object sender, RoutedEventArgs e)
+        private void select_cont_lease_Checked(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void select_cont_lease(object sender, RoutedEventArgs e)
-        {
-
-        }
-             
+        
+        
 	}
 }
