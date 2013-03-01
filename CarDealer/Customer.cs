16818873eb3c077cd8c7b835d152671dd1422870
Alejandro Jorge <alxnek@gmail.com>
2013-03-01 20:53:15 +0100

    Customer updated

--- CarDealer/Customer.cs
+++ CarDealer/Customer.cs
@@ -7,5 +7,47 @@ namespace CarDealerLibraries
 {
     public class Customer
     {
+        private string address;
+        private int phone;
+
+        /// <summary>
+        /// Create a customer
+        /// </summary>
+        /// <remarks>customer</remarks>
+        public Customer(string address, int phone)
+        {
+            this.address = address;
+            this.phone = phone;
+        }
+
+        /// <summary>
+        /// Get/Set fields
+        /// </summary>
+        /// <remarks>get/set</remarks>
+        public string Address
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Get/Set phone
+        /// </summary>
+        /// <remarks>get/set</remarks>
+        public int Phone
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
     }
 }
