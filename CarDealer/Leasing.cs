939c8caf0ba948c4130598c0cfedf0c188eac996
Alejandro Jorge <alxnek@gmail.com>
2013-03-01 12:23:15 +0100

    Leasing class finished

--- CarDealer/Leasing.cs
+++ CarDealer/Leasing.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Runtime.Serialization;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace CarDealerLibraries
 {
@@ -13,17 +14,19 @@ namespace CarDealerLibraries
         private IFormatter formatter;
         private Stream stream;
         private Truck truck;
-        private string nameOfContract;
+        private string contractName;
 
         /// <summary>
         /// Create a lease
         /// </summary>
         /// <remarks>Business and a truck</remarks>
-        public Leasing()
+        public Leasing(Truck truck, string contractName)
         {
-            throw new System.NotImplementedException();
+            this.formatter = new BinaryFormatter();
+            this.truck = truck;
+            this.contractName = contractName;
         }
-        //private string leaseName="file";
+
 
         /// <summary>
         /// Save the lease into a binary serialized file
@@ -34,15 +37,25 @@ namespace CarDealerLibraries
             //ask in the GUI for the string information of the lease
             //ask for the name of the binary file
             //save the bin file
+            this.stream = new FileStream(this.contractName + ".bin", FileMode.Create, FileAccess.Write, FileShare.None);
+            formatter.Serialize(stream, this.truck);
+            stream.Close();
         }
 
         /// <summary>
         /// Load an existing lease
         /// </summary>
         /// <remarks>IO</remarks>
-        public void LoadLease()
+        public static Leasing LoadLease(string nameOfContract)
         {
-            throw new System.NotImplementedException();
+            nameOfContract += ".bin";
+            Stream stream = new FileStream(nameOfContract, FileMode.Open, FileAccess.Read, FileShare.Read);
+            IFormatter formatter = new BinaryFormatter();
+
+            Truck truckOfLeasing = (Truck)formatter.Deserialize(stream);
+            stream.Close();
+
+            return new Leasing(truckOfLeasing, nameOfContract);
         }
 
         /// <summary>
@@ -51,7 +64,8 @@ namespace CarDealerLibraries
         /// <remarks>IO</remarks>
         public void PrintLease()
         {
-            throw new System.NotImplementedException();
+            //Change to show it in GUI
+            Console.Out.WriteLine("CONTRACT NAME: " + this.contractName + "\n" + this.truck.ToString());
         }
     }
 }
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -31,32 +31,32 @@ namespace ConsoleApplication1
                 list.Add(s);
                 list.Add(l);
                 list.Add(t);
-                CarDealerLibraries.CarDealer cdList = new CarDealerLibraries.CarDealer(list);               
-               Console.Out.WriteLine(cdList.ToString());
-               cdList.DeleteVehicle(c);
-               Console.Out.WriteLine("Delete Fiesta\n"+cdList.ToString());
+                CarDealerLibraries.CarDealer cdList = new CarDealerLibraries.CarDealer(list);
+                Console.Out.WriteLine(cdList.ToString());
+                cdList.DeleteVehicle(c);
+                Console.Out.WriteLine("Delete Fiesta\n" + cdList.ToString());
 
-               Console.Out.WriteLine("TEST SERIALIZABLE");
+                Console.Out.WriteLine("TEST SERIALIZABLE");
 
-               IFormatter formatter = new BinaryFormatter();
-               //Stream stream = new FileStream("MyFile.bin", FileMode.Create, FileAccess.Write, FileShare.None);
-               //formatter.Serialize(stream, c);
-               //stream.Close();
+                IFormatter formatter = new BinaryFormatter();
+                //Stream stream = new FileStream("MyFile.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+                //formatter.Serialize(stream, c);
+                //stream.Close();
 
-               //Once the file.bin is created you can load it again with
-               //Stream streamToRead = new FileStream("MyFile.bin", FileMode.Open, FileAccess.Read, FileShare.Read);
-               //CarDealerLibraries.Vehicle myFordFiesta = (CarDealerLibraries.Vehicle)formatter.Deserialize(streamToRead);
-               //streamToRead.Close();
+                //Once the file.bin is created you can load it again with
+                //Stream streamToRead = new FileStream("MyFile.bin", FileMode.Open, FileAccess.Read, FileShare.Read);
+                //CarDealerLibraries.Vehicle myFordFiesta = (CarDealerLibraries.Vehicle)formatter.Deserialize(streamToRead);
+                //streamToRead.Close();
 
-               //Console.Out.WriteLine("TEST SERIALIZABLE OF FORD FIESTA----"+ myFordFiesta.Model);
+                //Console.Out.WriteLine("TEST SERIALIZABLE OF FORD FIESTA----"+ myFordFiesta.Model);
 
-               CarDealerLibraries.Contract contract = new CarDealerLibraries.Contract(c,"fiesta");
-               contract.SaveContract();
-                
-               CarDealerLibraries.Contract.LoadContract("fiesta").PrintContract();
-                
-               
-               Console.In.ReadLine();
+                CarDealerLibraries.Contract contract = new CarDealerLibraries.Contract(c, "fiesta");
+                contract.SaveContract();
+
+                CarDealerLibraries.Contract.LoadContract("fiesta").PrintContract();
+
+
+                Console.In.ReadLine();
 
             }
         }
